4a0f88609066c2b15f644aa7c09fffc1c20920a4
amitpaultl <amitpaultl@gmail.com>
2023-01-20 21:49:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <ExteranUser></ExteranUser>
     </div>
   );
+}
+
+function ExteranUser(){
+  const [users, setUser] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=> res.json())
+    .then(data => setUser(data))
+  },[])
+  return(
+    <div>
+      <h2>Hello Babgladash</h2>
+      <p>{users.length}</p>
+      {users.map(user =><Dispaly name={user.name} emal={user.email}></Dispaly>)}
+      
+    </div>
+  )
+}
+
+const Dispaly = (props) =>{
+  return(
+    <div>
+      <h2>Name : {props.name}</h2>
+      <p>Email : {props.emal}</p>
+    </div>
+  )
 }
 
 export default App;
